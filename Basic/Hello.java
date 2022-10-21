83ad652bde38a5c87b8b43e534c420592e7c810a
armforward <freearm12345@gmail.com>
2022-10-21 11:06:58 +0700

    V2
    chaed 11

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,6 +1,8 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("My name Arm");
+        System.out.println("19");
         
     }
 }
